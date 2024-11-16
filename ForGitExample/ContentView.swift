23c2caea3d241c9d8daa860a272a050a1bcf2d19
Sabbir Hossain <sabbirsopnil07@gmail.com>
2024-11-16 14:28:49 +0600

    Initial Commit

---
+++ ForGitExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForGitExample
+//
+//  Created by Sopnil Sohan on 16/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForGitExample/ForGitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForGitExampleApp.swift
+//  ForGitExample
+//
+//  Created by Sopnil Sohan on 16/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ForGitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
